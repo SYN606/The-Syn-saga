fe0327adc224873f0a0c9b483eb62263db01c37b
SYN606 <postbox.501@yandex.com>
2024-05-21 21:50:02 +0530

    minor change in WSGI.py

--- synsaga/wsgi.py
+++ synsaga/wsgi.py
@@ -1,12 +1,7 @@
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "synsaga.settings")
 
-application = get_wsgi_application()
-
-app = application
-
-if os.environ.get('ENV') == 'production':
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+app = get_wsgi_application()
